350c1f38cb4ccad88f663a9742d20ad52d8183b5
NrupM <maryenorthrup@gmail.com>
2017-07-12 19:40:41 -0700

    added logic to checkForWin function

--- js/app.js
+++ js/app.js
@@ -6,6 +6,62 @@ function checkForWin() {
     if ($('#square1').text() === 'o' && $('#square2').text() === 'o'  &&  $('#square3').text() === 'o') {
         alert('o wins')
     }
+
+    if ($('#square1').text() === 'x' && $('#square5').text() === 'x'  &&  $('#square9').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square1').text() === 'o' && $('#square5').text() === 'o'  &&  $('#square9').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square1').text() === 'x' && $('#square4').text() === 'x'  &&  $('#square7').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square1').text() === 'o' && $('#square4').text() === 'o'  &&  $('#square7').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square2').text() === 'x' && $('#square5').text() === 'x'  &&  $('#square8').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square2').text() === 'o' && $('#square5').text() === 'o'  &&  $('#square8').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square3').text() === 'x' && $('#square5').text() === 'x'  &&  $('#square7').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square3').text() === 'o' && $('#square5').text() === 'o'  &&  $('#square7').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square3').text() === 'x' && $('#square6').text() === 'x'  &&  $('#square9').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square3').text() === 'o' && $('#square6').text() === 'o'  &&  $('#square9').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square4').text() === 'x' && $('#square5').text() === 'x'  &&  $('#square6').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square4').text() === 'o' && $('#square5').text() === 'o'  &&  $('#square6').text() === 'o') {
+        alert('o wins')
+    }
+
+    if ($('#square7').text() === 'x' && $('#square8').text() === 'x'  &&  $('#square9').text() === 'x') {
+        alert('x wins')
+    }
+
+    if ($('#square7').text() === 'o' && $('#square8').text() === 'o'  &&  $('#square9').text() === 'o') {
+        alert('o wins')
+    }
 }
 
 // wait for the DOM to finish loading
